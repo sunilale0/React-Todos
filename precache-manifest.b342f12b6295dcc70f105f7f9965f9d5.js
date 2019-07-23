0c77d11b8e6198b7602d8ae775f74f3fbc3dd05e
Student <jsitbon@qcc.cuny.edu>
2019-07-23 15:26:49 -0400

    Updates

---
+++ precache-manifest.b342f12b6295dcc70f105f7f9965f9d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b25c10a57b92cc92fddb164f167bee",
+    "url": "/React-Todos/index.html"
+  },
+  {
+    "revision": "2d639a2f454c0df111a9",
+    "url": "/React-Todos/static/css/main.542635df.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/React-Todos/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "2d639a2f454c0df111a9",
+    "url": "/React-Todos/static/js/main.9662e602.chunk.js"
+  },
+  {
+    "revision": "4dbcc2a58eaf7cb8c9f6",
+    "url": "/React-Todos/static/js/runtime~main.4a7ee91e.js"
+  }
+]);
